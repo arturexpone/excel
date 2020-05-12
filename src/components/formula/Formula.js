3d0743346ece337ad156adca400fdef3a56737f0
arturexpone <arturexpone@yandex.ru>
2020-05-12 19:04:59 +0300

    made a structure for events

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,6 +3,13 @@ import {ExcelComponent} from '@core/ExcelComponent';
 export class Formula extends ExcelComponent {
     static className = 'excel__formula';
 
+    constructor($root) {
+        super($root, {
+            name: 'Formula',
+            listeners: ['input', 'click'],
+        });
+    }
+
     toHTML() {
         return `
         <div class="info">
@@ -12,5 +19,9 @@ export class Formula extends ExcelComponent {
         <div class="input" contenteditable="true" spellcheck="false">
 
         </div>`
+    }
+
+    onInput(event) {
+        console.log('Formula: onInput', event)
     }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,16 @@
 import {DomListener} from './DomListener';
 
 export class ExcelComponent extends DomListener {
+    constructor($root, options = {}) {
+        super($root, options.listeners);
+    }
+
     // returns the component template
     toHTML() {
         return ''
+    }
+
+    init() {
+        this.initDOMListeners()
     }
 }
