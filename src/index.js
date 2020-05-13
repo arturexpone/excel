83afda01f41828651e8ae7e45f5791b4afe453c8
arturexpone <arturexpone@yandex.ru>
2020-05-13 21:45:35 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import {createStore} from '@core/createStore';
+import {rootReducer} from '@/redux/rootReducer';
 import {Excel} from '@/components/excel/Excel';
 import {Header} from '@/components/header/Header';
 import {Toolbar} from '@/components/toolbar/Toolbar';
@@ -5,9 +7,11 @@ import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 import './scss/index.scss'
 
+const store = createStore(rootReducer);
 
 const excel = new Excel('#app', {
     components: [Header, Toolbar, Formula, Table],
+    store: store,
 });
 
 excel.render();
