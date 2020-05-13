a4938238d8e8b75c0341c9452e682ec6d44739eb
arturexpone <arturexpone@yandex.ru>
2020-05-13 12:05:59 +0300

    defined elements for the resize

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -12,20 +12,14 @@ export class Formula extends ExcelComponent {
 
     toHTML() {
         return `
-        <div class="info">
-            fx
-        </div>
+        <div class="info">fx</div>
         
-        <div class="input" contenteditable="true" spellcheck="false">
-
-        </div>`
+        <div class="input" contenteditable="true" spellcheck="false"></div>`
     }
 
     onInput(event) {
-        console.log('Formula: onInput', event.target.textContent.trim())
     }
 
     onClick() {
-        console.log('mk')
     }
 }
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -39,6 +39,5 @@ export class Toolbar extends ExcelComponent {
     }
 
     onClick(event) {
-        console.log(event.target)
     }
 }
