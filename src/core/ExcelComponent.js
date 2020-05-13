b85455bbe2391acac06e929279b441ccbc6fb502
arturexpone <arturexpone@yandex.ru>
2020-05-13 20:38:59 +0300

    fixed the relationship between the formula and the cell

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,7 +5,7 @@ export class ExcelComponent extends DomListener {
         super($root, options.listeners);
         this.name = options.name || '';
         this.emitter = options.emitter;
-
+        this.unsubscribers = [];
         this.prepare()
     }
 
@@ -13,9 +13,17 @@ export class ExcelComponent extends DomListener {
 
     }
 
-    // returns the component template
     toHTML() {
         return ''
+    }
+
+    $emit(event, ...args) {
+        this.emitter.emit(event, ...args);
+    }
+
+    $on(event, fn) {
+        const unsub = this.emitter.subscribe(event, fn);
+        this.unsubscribers.push(unsub);
     }
 
     init() {
@@ -23,6 +31,7 @@ export class ExcelComponent extends DomListener {
     }
 
     destroy() {
-        this.removeDOMListeners()
+        this.removeDOMListeners();
+        this.unsubscribers.forEach(unsub => unsub());
     }
 }
