28354fa7106c532e8b01a1598f52fbb5a097b828
arturexpone <arturexpone@yandex.ru>
2020-05-14 14:26:11 +0300

    fix initial state, added saving the row size in local storage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {createStore} from '@core/createStore';
 import {storage} from '@core/utils';
 import {rootReducer} from '@/redux/rootReducer';
+import {initialState} from '@/redux/initialState';
 import {Excel} from '@/components/excel/Excel';
 import {Header} from '@/components/header/Header';
 import {Toolbar} from '@/components/toolbar/Toolbar';
@@ -8,7 +9,7 @@ import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 import './scss/index.scss'
 
-const store = createStore(rootReducer, storage('excel-state'));
+const store = createStore(rootReducer, initialState);
 
 store.subscribe(state => {
     storage('excel-state', state);
